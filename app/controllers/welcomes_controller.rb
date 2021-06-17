54a51549dcc5a531e67fc42e2eaa2a0cacfc8936
Patrick <pdn6991@gmail.com>
2021-06-17 00:26:28 -0400

    Added about path

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,11 @@
 class WelcomesController < ApplicationController
   def hello_method
-    render json: {message: "hello"}
+    render json: {message: "Hello from welcomes controller!"}
+  end
+
+  def about_method
+    message = "I guess my favorite computing language is ruby. I know the most about it, and it's what we're learning now. I've learned a little bit of JS but I didn't like it as much... too many ; and {}."
+    render json: {message: message}
+
   end
 end
